04239157aaecc033530811ec62b2cde9e37eab63
libitx <libitx@protonmail.com>
2020-07-16 17:01:49 +0100

    Patch bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Manic.MixProject do
   def project do
     [
       app: :manic,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
